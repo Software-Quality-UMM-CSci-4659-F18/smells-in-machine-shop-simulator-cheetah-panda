104f9935e665819df64ed9e7ea2285e1259a8a67
yoaku005 <yoaku005@d.umn.edu>
2018-09-25 15:17:11 -0500

    Make 'JobCompletionData.java' an inner class
    We moved 'JobCompletionData.java' to be an
    inner class of 'Job.java' as the first step
    to integrate the two classes.

--- src/applications/Job.java
+++ src/applications/Job.java
@@ -50,4 +50,27 @@ class Job {
         return id;
     }
 
+    public static class JobCompletionData {
+        private final int completionTime;
+        private final int totalWaitTime;
+        private final int jobNumber;
+
+        public JobCompletionData(int jobNumber, int completionTime, int totalWaitTime) {
+            this.jobNumber = jobNumber;
+            this.completionTime = completionTime;
+            this.totalWaitTime = totalWaitTime;
+        }
+
+        public int getCompletionTime() {
+            return completionTime;
+        }
+
+        public int getTotalWaitTime() {
+            return totalWaitTime;
+        }
+
+        public int getJobNumber() {
+            return jobNumber;
+        }
+    }
 }
