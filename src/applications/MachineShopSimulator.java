85479a25b566d46e3f06098fe49a0a17eb24a488
isaacyoa <35476418+isaacyoa@users.noreply.github.com>
2018-10-07 16:24:50 -0500

    Clean specification message chain in MachineShop
    We created a method in `SimulationSpecification`
    which removes the need to get `JobSpecification`
    in `MachineShopSimulator`. This cleans up the
    message chains in MachineShop but doesn't
    eliminate them.

--- src/applications/MachineShopSimulator.java
+++ src/applications/MachineShopSimulator.java
@@ -82,8 +82,8 @@ public class MachineShopSimulator {
             // create the job
             theJob = new Job(i);
             for (int j = 1; j <= tasks; j++) {
-                int theMachine = specification.getJobSpecifications(i).getSpecificationsForTasks()[2*(j-1)+1];
-                int theTaskTime = specification.getJobSpecifications(i).getSpecificationsForTasks()[2*(j-1)+2];
+                int theMachine = specification.getJobSpecificationTasks(i)[2*(j-1)+1];
+                int theTaskTime = specification.getJobSpecificationTasks(i)[2*(j-1)+2];
                 if (j == 1)
                     firstMachine = theMachine; // job's first machine
                 theJob.addTask(theMachine, theTaskTime); // add to
@@ -100,7 +100,7 @@ public class MachineShopSimulator {
             machine[i] = new Machine();
     }
 
-    /** load first jobs onto each machine
+    /** load first jobs onto each machineintially
      * @param specification*/
     static void startShop(SimulationSpecification specification) {
         // Move this to startShop when ready
